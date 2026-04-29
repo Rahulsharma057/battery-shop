0df4fe981f62fafc8ba54c37d84639ec308a1121
Vijay Singh <vijay.singh@staqo.com>
2026-04-29 11:12:02 +0530

    db fix

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
- const connectDB = require("./config/db");
-
 dotenv.config();
+
+ const connectDB = require("./config/db");
+ connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
- connectDB();
+
 
 console.log("🔥 SERVER STARTED");
 
